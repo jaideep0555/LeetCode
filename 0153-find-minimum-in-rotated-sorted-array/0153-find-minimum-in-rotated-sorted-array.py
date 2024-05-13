00281c9aad44c391c078dffdc9bb6c211841b6a3
Jaideep Janapati <66101948+jaideep0555@users.noreply.github.com>
2024-05-13 08:29:31 -0500

    Time: 50 ms (11.07%), Space: 16.9 MB (22.4%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -3,6 +3,9 @@ class Solution:
         minimum=float(inf)
         l,r=0,len(nums)-1
         while(l<=r):
+            if nums[l] < nums[r]:
+                minimum = min(minimum, nums[l])
+                break
             mid=(l+r)//2
             minimum=min(nums[mid],minimum)
             if(nums[mid]>=nums[r]):
